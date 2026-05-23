da837b428a5ec2f92994c3d853c381587cd6d61e
leadslot <maxijrodriguez09@gmail.com>
2026-05-23 03:01:47 -0300

    chore: remove debug-meta endpoint

--- apps/web/src/app/api/debug-meta/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from 'next/server'
-export async function GET() {
-  const secret = process.env.META_APP_SECRET
-  return NextResponse.json({ secret_set: !!secret, secret_length: secret?.length ?? 0, app_id: process.env.META_APP_ID })
-}
